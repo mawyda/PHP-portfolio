18c3ed04557414f7edf7232a8b77a307c1c8aa38
Matt Dalton <mdalton142@gmail.com>
2019-03-09 16:20:11 -0500

    Navbar CSS cleaned up. Pre-Blogs CSS

--- templates/footer.php
+++ templates/footer.php
@@ -1,7 +1,9 @@
 <html>
 	<!-- Footer -->
 	<div id="footer">
-		<a href="mailto: Mdalton142@gmail.com" title="Email Me!">Contact</a>
-		<a href="https://github.com/oasis142" title="Github" target="_blank">Github</a>
+		<div id="nav_left">
+			<a href="mailto: Mdalton142@gmail.com" title="Email Me!">Contact</a>
+			<a href="https://github.com/oasis142" title="Github" target="_blank">Github</a>
+		</div>
 	</div>
 </html>
